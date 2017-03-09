18caf6a84b64c3adfaff38ac7118b35ddbb95775
doun <doun@163.com>
2017-03-10 00:38:45 +0800

    导航基本OK，要将app切换成Main

---
+++ src/components/main/App.vue
@@ -0,0 +1,39 @@
+<template lang='pug'>
+  el-row
+    el-col(:span = 20, :offset = 2)
+      el-row#top(type = 'flex', align = 'middle')
+        el-col#menu(:span = 11, :offset = 1)
+          el-menu(theme='dark', mode='horizontal')
+            el-menu-item(index = '/') 库存
+            el-menu-item(index = '123') 入库
+            el-menu-item(index = '123') 盘点 
+        el-col#actions.is-align-middle(:span = 8, :offset = 4, :gutter = 10)
+          el-row(:gutter = 10)
+            el-col(:span = 16)
+              el-input(placeholder='扫描条码')
+            el-col(:span = 7)
+              building(v-model = "SelectedBuilding", :buildings = "buildings")
+      el-row#main
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import {Component as comp, Prop as prop} from 'vue-property-decorator'
+
+@comp
+export default class App extends Vue{
+  buildings = ['AL','LAL']
+  SelectedBuilding = 'AL'
+}
+</script>
+<style lang='stylus'>
+dark = #324157
+#top
+  background-color: dark 
+  margin-bottom: 10px
+  margin-left: 0
+  margin-right: 0 
+#main
+  background-color: dark
+  height: 200px
+</style>
