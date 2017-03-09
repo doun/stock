742c1eecae26e4f02da72fbdff1516c45af32427
doun <doun@163.com>
2017-03-10 00:07:38 +0800

    添加menus

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,19 @@
-<template>
-  <div>
-    <building :buildings = "buildings" :selected = "SelectedBuilding"></building>
-  </div>
+<template lang='pug'>
+  el-row
+    el-col(:span = 20, :offset = 2)
+      el-row#top(:gutter = 10, type = 'flex', align = 'middle')
+        el-col#menu(:span = 12)
+          el-menu(theme='dark', mode='horizontal')
+            el-menu-item(index = '/') 库存
+            el-menu-item(index = '123') 入库
+        el-col#actions.is-align-middle(:span = 8, :offset = 4)
+          el-col(:span = 14)
+            el-form
+              el-form-item
+                el-input(placeholder='扫描条码')
+          el-col(:span = 10)
+            building(:selected = "SelectedBuilding", :buildings = "buildings")
+      el-row#main
 </template>
 
 <script lang='ts'>
@@ -14,3 +26,8 @@ export default class App extends Vue{
   SelectedBuilding = 'AL'
 }
 </script>
+<style lang='stylus'>
+dark = #324157
+#top
+  background-color: dark 
+</style>
