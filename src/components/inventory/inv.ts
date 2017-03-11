6f065d59c22ecea216a06c84cdae91402eaeaff2
doun <doun@163.com>
2017-03-11 08:06:27 +0800

    处理css和包的小问题

--- src/components/inventory/inv.ts
+++ src/components/inventory/inv.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import {Component as comp, Prop as prop} from 'vue-property-decorator'
 
+let listColumns = []
+let listDetailsColumns = []
+
 @comp
 class List extends Vue{
 
 }
+
 @comp
 class Editor extends Vue{
 
