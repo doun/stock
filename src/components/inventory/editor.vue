5e66a6aeb602905e983d2aed37587a2a2f483f8a
doun <doun@163.com>
2017-03-14 10:38:16 +0800

    修改button尺寸

--- src/components/inventory/editor.vue
+++ src/components/inventory/editor.vue
@@ -10,17 +10,18 @@ div
           el-input-number(v-model = 'item.count', :min = 1)
         el-form-item(label = '位置')
           el-autocomplete(v-model.number = 'item.location_id')
+          el-button(type='text',@click='addLocation', size = 'small') 添加
         el-form-item(label = '物项')
           el-autocomplete(v-model.number ='item.product_id', icon='edit', placeholder='十字码')
-          el-button(type='text',@click='showDlg') 添加
+          el-button(type='text',@click='addProd', size = 'small') 添加
         el-form-item(label = '批次')
           el-autocomplete(icon='edit', v-model.number = 'item.batch_id', placeholder='批号')
-          el-button(type = 'text') 添加
+          el-button(type = 'text', @click = 'addBatch', size = 'small') 添加
         el-form-item(label = '有效期')
           el-date-picker(v-model = 'item.expiry_date')
         el-form-item
           el-button(type='primary') 提交
-      el-dialog(ref='prod_dialog', modal)
+      el-dialog(ref='prod_dialog', title = '编辑物项' )
         el-form
           el-form-item(label='名称')
             el-input(v-model='product.name')
@@ -30,7 +31,10 @@ div
             el-input(v-model='product.unit')
           el-form-item(label='规格')
             el-input(v-model='product.spec')
+        span(slot='footer')
           el-button() 取消
+          el-button() 提交
+
 </template>
 <script lang='ts'>
 import {Editor} from './inv.ts'
