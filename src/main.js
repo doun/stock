a273391fb1d22c819419ff5cbe02773d65358775
doun <doun@163.com>
2017-03-15 18:34:43 +0800

    准备迁到rails下

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import ElementUI from 'element-ui'
 import comps from './components'
 import App from './components/main.vue'
 import route from './route'
-
+import res from 'vue-resource'
 
 Vue.use(ElementUI)
 Vue.use(comps)
+Vue.use(res)
+
+const rurl = 'http://localhost:3000/'
+
+function get_url(item, action = ""){
+  let name_maps = {
+    "loc": "locations",
+    "prod": "productions",
+    "batch": "batches",
+    "line": "product_stores"
+  }
+  action = action == "" ? "" : "/" + action
+  return rurl + name_maps[item] + action + ".json"
+}
+
 window.data = new Vue({
   props:[
     'SelectedBuilding',
@@ -17,13 +32,19 @@ window.data = new Vue({
     getBuildings(){
     },
     init(type, obj){
+      this.$http.get(get_url(type, 'new')).then((rsp)=>{
+        for(let k in obj){
+          obj[k] = rsp[k]
+        }
+      })
       Object.assign(obj, {name: 'asdf23'})
     },
     submit(type, data, update_source = true){
-      if(type == 'loc'){
-        data.name = 'asdf'
-      }
-      this.$message('updated..')
+      //data.id为非零，表明要进行update,使用不同的http method
+      let url = data.id > 0 ? get_url(type, data.id) : get_url(type)
+      debugger
+      let action = this.$http.post(url, data)
+      action.then((rsp)=> console.log(rsp))
  //     this.$http.put(url, data).then((rsp)=>{
  //       if(update_source){
  //         console.log('updating source...')
